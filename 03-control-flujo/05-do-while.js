37b5c7b7a6a86c7c8381f2fadd2a4302329d617c
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:18:14 -0300

    Update 05-do-while.js

--- 03-control-flujo/05-do-while.js
+++ 03-control-flujo/05-do-while.js
@@ -10,10 +10,13 @@ while (i < 4) {
     i++;
 }
 
-
+// La diferencia principal es que primero se ejecuta el código y después se evalúa la condición
 do {
     if (i % 2 === 0) {
         console.log(i);
     }
     i++;
 } while (i < 4);
+
+// While -> Se ejecuta 0 o N veces
+// Do While -> Se ejecuta 1 o N veces
