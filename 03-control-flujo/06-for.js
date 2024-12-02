a1e37eaeca022a5be2513d813c4305d26e61f0c0
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:24:08 -0300

    Update 06-for.js

--- 03-control-flujo/06-for.js
+++ 03-control-flujo/06-for.js
@@ -7,7 +7,7 @@ while (i < 2){
 }
 
 
-
+// El for es una forma resumida de While, aunque se utiliza cuando sabemos la cantidad exacta de veces que queremos que se ejecute
 for (let i = 0; i < 10; i++){
     if (i % 2 === 0){
         console.log('Numero par ',i);
