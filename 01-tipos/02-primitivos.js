dbbc115d879392e690e9a51062de51e50e835dcd
Maxi <maxinunez2455@gmail.com>
2024-12-02 15:54:47 -0300

    Update 02-primitivos.js

--- 01-tipos/02-primitivos.js
+++ 01-tipos/02-primitivos.js
@@ -6,8 +6,8 @@ let verdadero = true; // Boolean
 
 let falso = false;  // Boolean
 
-let noDefinido;     // Undefined
+let noDefinido;     // Undefined -> Comúnmente aparece cuando no se inicializa una variable
 
-let undef = undefined;  // Undefined puede ser usado como valor
+let undef = undefined;  // Undefined puede ser usado como valor 
 
-let nulo = null;    // Null es un valor nulo o vacio que se puede usar como valor
+let nulo = null;    // Null es un valor nulo o vacio que se puede usar como valor -> Se utiliza para indicar que la variable está vacía. Debe indicarse a diferencia de Undefined que aparece solo.
