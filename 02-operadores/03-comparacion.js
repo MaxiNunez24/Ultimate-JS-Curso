ba370cb49da85f40a140ddcc80172539525bfcb2
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:12:44 -0300

    Update 03-comparacion.js

--- 02-operadores/03-comparacion.js
+++ 02-operadores/03-comparacion.js
@@ -10,6 +10,6 @@ console.log(a <= 5); // Menor o igual que
 // Operadores de Igualdad
 console.log(a == 10); // Igual que
 console.log(a != 10); // Diferente que
-console.log(a == '10'); // Igual que  (compara solo el valor no el tipo de dato)
+console.log(a == '10'); // Igual que  (compara solo el valor, no el tipo de dato)
 console.log(a === '10'); // Igual que (compara el valor y el tipo de dato)
 console.log(a !== '10'); // Diferente que (compara el tipo de dato)
