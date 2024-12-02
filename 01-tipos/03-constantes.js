d04fcd0a5e39b06772251b0f02d1047ab3d62fad
Maxi <maxinunez2455@gmail.com>
2024-12-02 15:56:46 -0300

    Update 03-constantes.js

--- 01-tipos/03-constantes.js
+++ 01-tipos/03-constantes.js
@@ -2,14 +2,14 @@ let nombre = 'Hola Mundo';
 
 nombre = 'Jorge';  // cambiar el valor de la variable
 
-console.log(nombre); // Imprime el ultimo valor asignado a la variable
+console.log(nombre); // Imprime el valor asignado a la variable
 
-// Es recomendable nunca cambiar el valor de una variable,para eso usaremos const,
+// Es recomendable nunca cambiar el valor de una variable. Por eso se utilizan mayormente las Constantes -> const
 // en su lugar se debe crear otra variable con un nombre diferente.
 // Si por alguna razon se necesita cambiar el valor de una variable, se debe usar let.
 
-// variable a la cual no se le puede cambiar el valor
+// variable a la cual no se le puede cambiar el valor -> Constante
 const nombre2 = 'Hola Mundo';
 
-// variable a la cual se le puede cambiar el valor
+// variable a la cual se le puede cambiar el valor -> Variable
 let nombre3 = 'Hola Mundo';
