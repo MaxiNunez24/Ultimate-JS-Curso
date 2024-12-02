8448f1123685461dd0c8653a305a277c3c982281
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:00:36 -0300

    Update 06-objetos.js

--- 01-tipos/06-objetos.js
+++ 01-tipos/06-objetos.js
@@ -1,22 +1,22 @@
 // Personaje de TV
 let nombre = 'Homero Simpson';
-let anime = 'Los Simpson';
+let programa = 'Los Simpson';
 let edad = 40;
-
+// Objeto Literal -> {}
 let personaje = {
-    nombre: 'Homero Simpson', // A esto se le conoce como par llave-valor
-    anime: 'Los Simpson',
+    nombre: 'Homero Simpson', // A esto se le conoce como par llave-valor -> key-value
+    programa: 'Los Simpson',
     edad: 40,
-};  // Objeto
+};  // Objeto Literal 
 
 console.log(personaje); // Imprime el objeto completo
 
 console.log(personaje.nombre); // Imprime el valor de la llave nombre
 
-console.log(personaje['anime']); // Imprime el valor de la llave anime
+console.log(personaje['programa']); // Imprime el valor de la llave programa
 
 personaje.edad = 45; // Cambia el valor de la llave edad
 personaje['edad'] = 50; // Cambia el valor de la llave edad
 
-delete personaje.anime; // Elimina la llave anime
+delete personaje.programa; // Elimina la llave anime
 console.log(personaje); // Imprime el objeto completo
