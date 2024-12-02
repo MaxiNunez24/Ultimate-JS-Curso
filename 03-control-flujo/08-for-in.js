0c3203e407149b4a92070068414de5ad5de3ef7a
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:25:48 -0300

    Update 08-for-in.js

--- 03-control-flujo/08-for-in.js
+++ 03-control-flujo/08-for-in.js
@@ -4,6 +4,6 @@ let user = {
     age: 25,
 };
 
-for (let prop in user) {
+for (let prop in user) { // Asigna a prop cada una de las llaves/propiedades del objeto user
     console.log(prop, user[prop]);
 }
