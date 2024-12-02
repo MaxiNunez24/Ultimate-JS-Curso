3eb1dbe07b0e87c1eed7518a82136caca3697579
Maxi <maxinunez2455@gmail.com>
2024-12-02 16:09:52 -0300

    Update 09-argumentos.js

--- 01-tipos/09-argumentos.js
+++ 01-tipos/09-argumentos.js
@@ -1,9 +1,9 @@
-function suma(a, b) {
+function suma(a, b) { // Los valores que recibe una función se llaman Parámetros
     console.log(arguments); // Imprime los argumentos que se le pasan a la funcion, esta no es recomendable usarla
     return a + b;
 }
 
-let resultado = suma(5,6); // El valor que se le pasa a la funcion se le conoce como argumento
+let resultado = suma(5,6); // Los valores que se le pasan a una función se llaman Argumentos
 
 console.log(resultado);
-console.log(typeof suma); // Imprime el tipo de dato de la funcion
+console.log(typeof suma); // Imprime el tipo de dato de la funcion -> function
